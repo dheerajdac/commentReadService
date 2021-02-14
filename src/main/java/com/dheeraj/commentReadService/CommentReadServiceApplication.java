9a30e57bd6faea1a38ddf0ac060a376516a75068
dheeraj kumar <dheeradac@gmail.com>
2021-02-14 18:51:31 +0000

    first commit

---
+++ src/main/java/com/dheeraj/commentReadService/CommentReadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dheeraj.commentReadService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentReadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentReadServiceApplication.class, args);
+	}
+
+}
